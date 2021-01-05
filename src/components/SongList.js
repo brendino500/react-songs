4812b2b547a9c40b176ff682703f9b65527f5963
brendino500 <brenda.ty@live.com>
2021-01-05 12:51:50 +0000

    lectire 242- action creators from components

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
     // this.props === { songs: state.songs }
-    return <div>SongList</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 // all data inside redux store
+// anytime change to redux state, this function reruns with newly state object
 const mapStateToProps = (state) => {
   console.log(state);
-
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong: selectSong,
+})(SongList);
